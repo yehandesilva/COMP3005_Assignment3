f25dc87e73da1b55722ad9ff6194d3d611ccd4df
Yehan De Silva <yehan@fiberdock.ca>
2024-03-06 22:21:26 -0500

    DatabaseController boiler plate code

---
+++ src/main/java/DatabaseController.java
@@ -0,0 +1,27 @@
+public class DatabaseController {
+
+    public void setupTable() {
+
+    }
+
+    public void dropTable() {
+
+    }
+
+    public void getAllStudents() {
+
+    }
+
+    public void addStudent(String first_name, String last_name, String email, String enrollment_date) {
+
+    }
+
+    public void updateStudentEmail(int student_id, String new_email) {
+
+    }
+
+    public void deleteStudent(int student_id) {
+
+    }
+
+}
