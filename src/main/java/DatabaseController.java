6ea4aea5465b5aa265e52228e8ddbe9fcb24f535
Yehan De Silva <yehan@fiberdock.ca>
2024-03-06 22:27:12 -0500

    DatabaseController setup and termination

--- src/main/java/DatabaseController.java
+++ src/main/java/DatabaseController.java
@@ -1,11 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseController {
+
+    private Connection connection;
+    Statement statement;
+
+    public DatabaseController(String url, String user, String password) {
+        try {
+            this.connection = DriverManager.getConnection(url, user, password);
+            this.statement = this.connection.createStatement();
+            System.out.println("Connected to database");
+        } catch (Exception e) {
+            System.out.println("Connection to database failed");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
     public void setupTable() {
 
     }
 
-    public void dropTable() {
-
+    public void closeController() {
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public void getAllStudents() {
