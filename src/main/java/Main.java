02ce0bf7f4a108281ff8dcb5d80bef659ebcb065
Yehan De Silva <yehan@fiberdock.ca>
2024-03-06 23:47:28 -0500

    Added demonstration methods to main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
      * Print contents of an array list.
      * @param results Array to print contents of.
      */
-    private void displayResults(ArrayList<String> results) {
+    public static void displayResults(ArrayList<String> results) {
         for (String result: results) {
             System.out.println(result);
         }
@@ -29,7 +29,27 @@ public class Main {
 
         // Database controller commands
         DatabaseController controller = new DatabaseController(url, user, password);
+
+        // 1) Setup table and fill with sample data
         controller.setupTable();
+
+        // 2) Display sample data
+        Main.displayResults(controller.getAllStudents());
+
+        // 3) Add new student
+        controller.addStudent("Yehan", "De Silva", "yehandesilva@cmail.carleton.ca", "2024-01-01");
+
+        // 4) Update student email
+        controller.updateStudentEmail(1, "johndoe2@example.com");
+
+        // 5) Delete student
+        controller.deleteStudent(2);
+
+        // 6) Display new data
+        System.out.println();
+        Main.displayResults(controller.getAllStudents());
+
+        // 7)  connection
         controller.closeController();
 
     }
